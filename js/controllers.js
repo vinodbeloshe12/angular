20c566903ff828851ea3f099167205db17df56d3
Vinod Beloshe <vinodbeloshe12@gmail.com>
2016-01-28 13:08:19 +0530

    first commit

---
+++ js/controllers.js
@@ -0,0 +1,25 @@
+angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngSanitize', 'angular-flexslider'])
+
+.controller('HomeCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+  //Used to name the .html file
+  $scope.template = TemplateService.changecontent("home");
+  $scope.menutitle = NavigationService.makeactive("Home");
+  TemplateService.title = $scope.menutitle;
+  $scope.navigation = NavigationService.getnav();
+
+  $scope.mySlides = [
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
+    'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+  ];
+})
+
+.controller('headerctrl', function($scope, TemplateService) {
+  $scope.template = TemplateService;
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+    $(window).scrollTop(0);
+  });
+})
+
+;
---
+++ js/navigation.js
@@ -0,0 +1,31 @@
+var navigationservice = angular.module('navigationservice', [])
+
+.factory('NavigationService', function() {
+  var navigation = [{
+    name: "Home",
+    classis: "active",
+    link: "#/home",
+    subnav: [{
+      name: "Subnav1",
+      classis: "active",
+      link: "#/home"
+    }]
+  }];
+
+  return {
+    getnav: function() {
+      return navigation;
+    },
+    makeactive: function(menuname) {
+      for (var i = 0; i < navigation.length; i++) {
+        if (navigation[i].name == menuname) {
+          navigation[i].classis = "active";
+        } else {
+          navigation[i].classis = "";
+        }
+      }
+      return menuname;
+    },
+
+  };
+});
